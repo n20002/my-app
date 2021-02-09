f6c7f1c26bb16bdb253958860a8dba9efa425f2a
Rao Ikemura <n20002@std.it-college.ac.jp>
2021-02-09 08:03:15 +0000

    0209finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
+const fetch = require('node-fetch')
+
+function App () {
+  const [images, setImages] = useState([]) // 今なんの写真が表示されてるか、その状態を管理する
+  const [text, setText] = useState('') // 検索バーに入れる文字列の状態を管理
+  const [query, setQuery] = useState('') // 今なんの文字列で検索してるかを保持する
+  useEffect(() => {
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setImages(data.results) // 取ってきたデータをImagesに入れる
+      })
+  }, [query]) // 第二引数にqueryをセットする、これによりqueryの値が変更された時にuseEffectが走る。
+
+  const onSubmit = e => {
+    e.preventDefault() // 画面遷移を防ぐ
+    setQuery(text)
+    setText('') // まっさらにまた戻す
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='main'>
+        <form onSubmit={onSubmit}>
+          <label>Image search</label>
+          <input
+            type='text'
+            onChange={e => setText(e.target.value)} // e.target.valueでinputの値を取得できる
+            value={text}
+            placeholder='Please enter'
+          />
+          <button type='submit'>Search</button>
+        </form>
+      </div>
+      <div className='container'>
+        {images.map((
+          image // 繰り返し処理
+        ) => (
+          <div key={image.id} className='card'>
+            <img src={image.urls.regular} className='card-img' alt='' />
+            <div className='card-content'>
+              <div className='card-title'>{image.alt_description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
